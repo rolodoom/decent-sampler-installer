f9f6a640839a644a82a22206fa1929528467a1ab
rolodoom <rolodoom@gmail.com>
2022-11-14 10:25:04 -0500

    :art: Refactor code

--- main.sh
+++ main.sh
@@ -11,23 +11,35 @@
 # |_______| |_______|    Tested on Manjaro
 #
 #
-#
-#
 
-# test for required commands
-#command -v unzip >/dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Aborting."; exit 1; }
-
+############################################################
+# Constants                                                #
+############################################################
 TEMPDIR=$(readlink -f "@temp")
 VST=$(readlink -f "/usr/lib/vst")
 VST3=$(readlink -f "/usr/lib/vst3")
 APPBIN=$(readlink -f "/opt/DecentSampler")
+COMMAND_NAME="./main.sh"
 
-# install subrutine
-aa_install () {
-    echo "installing..."
+############################################################
+# Install Required                                         #
+############################################################
+CheckRequired () {
+    
+    # test for required commands
+    command -v unzip >/dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Aborting."; exit 1; }
 
     # installing required libcurl-gnutls
+    # echo "Installing required libcurl-gnutls package..."
     # sudo pacman -S libcurl-gnutls
+}
+
+############################################################
+# Install                                                  #
+############################################################
+Install () {
+    echo ""
+    echo "Installing DS Sampler..."
 
     # check if @temp exists
     if [ ! -d "$TEMPDIR" ]; then
@@ -35,7 +47,6 @@ aa_install () {
         mkdir @temp
         # Unzip Sofware
         tar -xf Decent*.tar.gz -C "$TEMPDIR"
-        
     fi
 
     # clean mac Files from installers
@@ -54,13 +65,10 @@ aa_install () {
     # copy Stand Alone
     sudo cp -r "$TEMPDIR"/Decent*/DecentSampler "$APPBIN"
 
-
     #chmod 777
     sudo chmod +x "$APPBIN"/DecentSampler
 
-
     #usr/local/bin links
-
     sudo ln -s "$APPBIN"/DecentSampler "/usr/local/bin/DecentSampler"
 
     # copy .local files
@@ -70,67 +78,100 @@ aa_install () {
     sudo rm -rf "$TEMPDIR"
 
     echo "Done!!!"
+    echo ""
 }
 
-# uninstall subrutine
-aa_uninstall () {
-    echo "Uninstalling..."
+
+############################################################
+# Uninstall                                                #
+############################################################
+Uninstall () {
+
+    echo ""
+    echo "Unistalling DS Sampler..."
 
     # @temp
     sudo rm -rf "$TEMPDIR"
 
     # desktop files
-
     sudo rm -rf /usr/share/applications/DecentSampler.desktop
 
     # icons
     sudo rm -rf /usr/share/icons/DecentSampler.png
 
-
     # vst
     sudo rm -rf "$VST"/DecentSampler.so
     sudo rm -rf "$VST3"/DecentSampler.vst3
+
     # bin
     sudo rm -rf "$APPBIN"
-
     sudo rm -rf /usr/local/bin/DecentSampler
 
     echo "Done!!!"
+    echo ""
 }
 
 
-# Start
-echo ""
-echo "**************************************"
-echo "*       Decent Sampler Installer     *"
-echo "**************************************"
-echo "Available commands:"
-echo ""
-echo "  [I]nstall"
-echo "  [U]ninstall"
-echo "  [Q]uit"
-echo ""
-echo -n "Command [I,U,Q]:"
-read input
-echo ""
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo
+   echo "Usage: $COMMAND_NAME [OPTIONS]"
+   echo
+   echo "Install Decent Sampler for Linux."
+   echo
+   echo "Options:"
+   echo "  -h     Print this Help."
+   echo "  -i     Install DS Sampler."
+   echo "  -u     Uninstall DS Sample."
+   echo
+}
 
-# 
-case "$input" in
-    install|INSTALL|I|i)
-    aa_install    
-    ;;
+############################################################
+# InvalidOption                                            #
+############################################################
+InvalidOption()
+{
+   # Display Invalid Option Text
+   echo
+   echo "$COMMAND_NAME: invalid option."
+   echo "See '$COMMAND_NAME -h'"
+   echo
+}
 
-    uninstall|UNINSTALL|U|u)
-    aa_uninstall
-    ;;
-      
-    quit|QUIT|Q|q|exit|EXIT|x|X) 
-    echo "Bye!!!"
-    ;;
-    
-    # NOT IMPLEMENTED
-    *)
-    echo "Choose an available command!!!"
-    ;;
+############################################################
+# Main Program                                             #
+############################################################
 
-esac
+# No option supplied
+if [ $# -eq 0 ]
+    then
+        Help
+        exit;
+fi
+
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts ":hiu" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      i) # Install DS Sample
+         CheckRequired
+         Install
+         exit;;
+      u) # Uninstall DS Sampler
+         Uninstall
+         exit;;
+      \?) # Invalid option
+         InvalidOption
+         exit;;
+   esac
+done
